12b5ed7021a87f1689f999ec9f1553e19c0fe0e4
cla6shade <cla6shade@github.com>
2025-05-25 23:36:39 +0900

    chore: shadcn 컴포넌트 추가

--- app/components/ui/avatar.tsx
+++ app/components/ui/avatar.tsx
@@ -1,9 +1,9 @@
-"use client"
+
 
 import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
-import { cn } from "~/lib/utils"
+import { cn } from "@/lib/utils"
 
 function Avatar({
   className,
@@ -34,20 +34,4 @@ function AvatarImage({
   )
 }
 
-function AvatarFallback({
-  className,
-  ...props
-}: React.ComponentProps<typeof AvatarPrimitive.Fallback>) {
-  return (
-    <AvatarPrimitive.Fallback
-      data-slot="avatar-fallback"
-      className={cn(
-        "bg-muted flex size-full items-center justify-center rounded-full",
-        className
-      )}
-      {...props}
-    />
-  )
-}
-
-export { Avatar, AvatarImage, AvatarFallback }
+export { Avatar, AvatarImage }
